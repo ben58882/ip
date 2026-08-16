402e5aa5e0ed33f8afbc701ee03dede28440b8cc
Benjamin Ng <benjamin.ngzy@gmail.com>
2026-08-16 20:52:13 +0800

    Changed chatbot name from Duke to BENBOT

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# BenBot User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/BenBot.java
@@ -0,0 +1,17 @@
+/** The entry point for the BenBot chatbot application. */
+public class BenBot {
+    /** Starts BenBot and displays its welcome message. */
+    public static void main(String[] args) {
+        String banner = " ____              ____        _   \n"
+                + "| __ )  ___ _ __  | __ )  ___ | |_ \n"
+                + "|  _ \\ / _ \\ '_ \\ |  _ \\ / _ \\| __|\n"
+                + "| |_) |  __/ | | || |_) | (_) | |_ \n"
+                + "|____/ \\___|_| |_||____/ \\___/ \\__|\n";
+
+        System.out.println(banner);
+        System.out.println("Hello! I'm BenBot.");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
